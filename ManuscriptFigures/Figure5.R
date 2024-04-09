29be16f34dcbf77b1c84fb5a49c340fefd3ba330
Nurdan Kuru <68369488+nurdannkuru@users.noreply.github.com>
2024-04-09 21:54:05 +0300

    Update Figure5.R

--- ManuscriptFigures/Figure5.R
+++ ManuscriptFigures/Figure5.R
@@ -4,10 +4,20 @@ library(ggsignif)
 algs <- c("DCA", "GaussDCA", "PSICOV", "MIp")
 cols <- c("#F8766D", "#C49A00", "#53B400", "#00C094", "#00B6EB", "#A58AFF", "#FB61D7")
 
+files <- list.files("/Users/nurdankuru/Desktop/PHACE_Paper/MSAbased")
+
 plot_list <- list()
 for (ind in 1:length(algs)){
   alg <- algs[ind]
-  load(sprintf("/Users/nurdankuru/Desktop/Paper_ROC_Measures_%s.RData", alg))
+  loc <- grep(alg, files)[1]
+  
+  if (ind==4){
+    alg2 <- "ZMIp"
+  } else {
+    alg2 <- alg
+  }
+  load(sprintf("/Users/nurdankuru/Desktop/PHACE_Paper/MSAbased/%s", files[loc]))
+  print(files[loc])
   
   vect_main <- vect_all
   vect_main <- as.data.frame(vect_main)
@@ -27,7 +37,7 @@ for (ind in 1:length(algs)){
       vect <- vect_main[which(vect_main$THR==thr),]
       
       P1 <- vect[which(vect[,1]=="PHACE"),]
-      P2 <- vect[which(vect[,1]==alg),]
+      P2 <- vect[which(vect[,1]==alg2),]
       
       val1 <- as.numeric(P1$AUC)
       val2 <- as.numeric(P2$AUC)
@@ -49,15 +59,15 @@ for (ind in 1:length(algs)){
         labs(title = "",
              x = "", y = "AUC") +
         theme(legend.position = "top",
-              axis.text.x = element_text(size = 12),   
+              axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 12),
               axis.text.y = element_text(size = 12),  
               axis.title.x = element_text(size = 14), 
               axis.title.y = element_text(size = 14)) + 
         scale_color_manual(values = color_vector) +
-        geom_signif(comparisons = list(c("PHACE", "DCA")), textsize = 8, vjust = -0.5, step_increase = 0.05, map_signif_level = TRUE, color = "black") +
+        geom_signif(comparisons = list(c("PHACE", alg)), textsize = 8, vjust = -0.5, step_increase = 0.05, map_signif_level = TRUE, color = "black") +
         theme(legend.position = "topright") +
         coord_cartesian(ylim = c(0, 1.09))
-      
+
       plot_list[[sprintf("%s_Thr%s", alg, thr)]] <- p
     }
   }
@@ -83,5 +93,7 @@ combined_plot_Thr5 <- plot_grid(plotlist = plot_list_Thr5, ncol = 4, labels = ""
 combined_final <- plot_grid(combined_plot_Thr0, combined_plot_Thr5, nrow = 2, labels = c("A", "B"))
 print(combined_final)
 
-ggsave(sprintf("All_ROC_Paper_Pairwise.pdf"), combined_final, width = ( 210/25.4), height = 4)
+ggsave(sprintf("All_ROC_Paper_Pairwise.pdf"), combined_final, width = ( 210/25.4), height = 8)
+
+
 
