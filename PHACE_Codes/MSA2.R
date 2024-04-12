f14cfab0325e775dd6ae811cd05a960593b0f46d
Nurdan Kuru <68369488+nurdannkuru@users.noreply.github.com>
2024-04-12 19:34:07 +0300

    Create MSA2.R

---
+++ PHACE_Codes/MSA2.R
@@ -0,0 +1,39 @@
+library(ape)
+library(Biostrings)
+library(tidytree)
+library(stringr)
+library(dplyr)
+library(bio3d)
+
+args = commandArgs(trailingOnly=TRUE)
+
+id <- args[1]
+
+file_fasta <- sprintf("%s_MaskedMSA.fasta", id)
+
+# Read fasta file, MSA
+fasta <- read.fasta(file = file_fasta)
+msa <- fasta$ali
+
+for (i in 1:length(msa[1,])){
+  gap <- which(msa[,i]=="-")
+  oth <- which(msa[,i]!="-")
+  
+  msa[gap,i] <- "G"
+  msa[oth,i] <- "C"
+}
+
+num_leaves <- length(msa[,1])
+vect <- matrix(0, (num_leaves*2), 1)
+k <- 0
+for (i in seq(1, (num_leaves*2), 2)){
+  k <- k + 1
+  vect[i, 1] <- paste(">", row.names(msa)[k], sep = "")
+}
+k <- 0
+for (i in seq(2, (num_leaves*2), 2)){
+  k <- k + 1
+  vect[i, 1] <- paste(msa[k,], collapse = "")
+}
+
+write.table(vect, quote = F, col.names = F, row.names = F, sprintf("MSAs/%s_Upd_Gap.fasta", id))
